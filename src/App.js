edf4e581cdd1574cb7bb1935be6431b8ce1e34ac
Richard McDaniel <dannypride1@gmail.com>
2023-02-09 14:50:49 -0800

    JS practice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="DandDDude.webp" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <ul>
+          <li><a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React Hello World
+          </a></li>
+          <li><a
+            className="App-link"
+            href="https://www.google.com"
+            target="_blank"
+          >
+            Let me google that for ya
+          </a></li>
+        </ul>
+        <ul>
+          <li>Apples</li>
+          <li>Bananas</li>
+          <li>Milk
+            <ul>
+              <li>2%</li>
+              <li>Chocolate</li>
+            </ul>
+          </li>
+        </ul>
       </header>
     </div>
   );
