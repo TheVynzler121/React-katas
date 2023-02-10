1593af813364521a9f3bfb08fd804b8a36efa1aa
Richard McDaniel <dannypride1@gmail.com>
2023-02-10 15:22:15 -0800

    React/JS/HTML practice

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
-
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  console.log("Rendering", counter); 
   return (
     <div className="App">
       <header className="App-header">
-        <img src="DandDDude.webp" className="App-logo" alt="logo" />
+        {counter}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button className="horse" onClick={() => {
+            console.log("clicked!");
+            
+            setCounter(counter - 2);
+            }}
+            >Click me</button>
         </p>
-
-        <ul>
-          <li><a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React Hello World
-          </a></li>
-          <li><a
-            className="App-link"
-            href="https://www.google.com"
-            target="_blank"
-          >
-            Let me google that for ya
-          </a></li>
-        </ul>
-        <ul>
-          <li>Apples</li>
-          <li>Bananas</li>
-          <li>Milk
-            <ul>
-              <li>2%</li>
-              <li>Chocolate</li>
-            </ul>
-          </li>
-        </ul>
       </header>
     </div>
   );
